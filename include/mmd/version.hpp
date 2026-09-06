c8c3e60605c3f23b34ed18447572a288218b4ef0
haturatu <taro@eyes4you.org>
2026-09-06 15:30:39 +0900

    fix: skip sdk tests in subprojects

--- include/mmd/version.hpp
+++ include/mmd/version.hpp
@@ -2,8 +2,8 @@
 
 #define LIBMMD_VERSION_MAJOR 0
 #define LIBMMD_VERSION_MINOR 1
-#define LIBMMD_VERSION_PATCH 0
+#define LIBMMD_VERSION_PATCH 1
 
 namespace mmd {
-inline constexpr const char* version = "0.1.0";
+inline constexpr const char* version = "0.1.1";
 }
